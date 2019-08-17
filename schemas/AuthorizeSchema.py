d532b4439ed5067db0d2957ff2ddb921dd232fb7
viniciusvnr <viniciusvnr@gmail.com>
2019-08-17 16:05:55 -0300

    update

--- schemas/AuthorizeSchema.py
+++ schemas/AuthorizeSchema.py
@@ -6,4 +6,4 @@ from schemas.TransactionSchema import TransactionSchema
 class AuthorizeSchema(Schema):
     account = fields.Nested(AccountSchema, required=True)
     transaction = fields.Nested(TransactionSchema, required=True)
-    latesttransactions = fields.List(fields.Nested(TransactionSchema), required=True)
+    lasttransactions = fields.List(fields.Nested(TransactionSchema), required=True)
--- services/TransactionValidator.py
+++ services/TransactionValidator.py
@@ -20,7 +20,7 @@ def validate_rules(input_obj):
     card_is_active = input_obj["account"]["cardIsActive"]
     account_limit = input_obj["account"]["limit"]
     deny_list = input_obj["account"]["denylist"]
-    latest_transactions = input_obj["latesttransactions"]
+    last_transactions = input_obj["lasttransactions"]
     transaction_merchant = input_obj["transaction"]["merchant"]
     transaction_amount = input_obj["transaction"]["amount"]
     transaction_time = input_obj["transaction"]["time"]
@@ -32,13 +32,13 @@ def validate_rules(input_obj):
     if not card_is_active:
         denied_reasons.append({"rule2": "No transaction should be approved when the card is blocked"})
 
-    if transaction_amount >= (account_limit * 0.9) and len(latest_transactions) == 0:
+    if transaction_amount >= (account_limit * 0.9) and len(last_transactions) == 0:
         denied_reasons.append({"rule3": "The first transaction shouldn't be above 90% of the limit"})
 
-    if latest_transactions:
+    if last_transactions:
         count = 0
 
-        for trn in latest_transactions:
+        for trn in last_transactions:
             if trn["merchant"] == transaction_merchant:
                 count += 1
 
@@ -48,16 +48,16 @@ def validate_rules(input_obj):
     if transaction_merchant in deny_list:
         denied_reasons.append({"rule5": "Merchant in deny list"})
 
-    if len(latest_transactions) >= 3:
+    if len(last_transactions) >= 3:
         count = 0
 
-        for i, trn in enumerate(latest_transactions):
+        for i, trn in enumerate(last_transactions):
             # timestamp da transacao da lista <latest_transactions>
             dt1 = trn["time"].timestamp()
             # timestamp da transacao do payload
             dt2 = transaction_time.timestamp()
             # timestamp do proximo item da lista <latest_transactions>
-            dt2_inner = latest_transactions[(i + 1) % len(latest_transactions)]["time"].timestamp()
+            dt2_inner = last_transactions[(i + 1) % len(last_transactions)]["time"].timestamp()
             dt_delta = dt2 - dt1
             dt_inner_delta = dt2_inner - dt1
 
