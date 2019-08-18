d1d130112f50ccf0b422b7ebe94de2f37d6d93ec
viniciusvnr <viniciusvnr@gmail.com>
2019-08-18 15:40:49 -0300

    remove config reference

--- run.py
+++ run.py
@@ -1,16 +1,10 @@
 from flask import Flask
 from api_v1 import api_bp_v1
-from app_config import config
 
 
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_object(config_filename)
-    app.register_blueprint(api_bp_v1)
-
-    return app
+app = Flask(__name__)
+app.register_blueprint(api_bp_v1)
 
 
 if __name__ == "__main__":
-    app = create_app(config)
     app.run(debug=True, host='0.0.0.0')
