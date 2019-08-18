e03b62da2c71a7b519510224b857a4995f387c68
viniciusvnr <viniciusvnr@gmail.com>
2019-08-18 10:38:05 -0300

    update

--- services/TransactionAuthorizationService.py
+++ services/TransactionAuthorizationService.py
@@ -1,17 +1,21 @@
 from .TransactionValidator import validate_rules
 
 
+def calculate_new_limit(transactions_status, account_limit, transaction_amount):
+
+    if transactions_status:
+        return account_limit - transaction_amount
+
+    return account_limit
+
+
 def authorize_transaction(authorization_request):
     # Call Rule validator
     val_rules = validate_rules(authorization_request)
     account_limit = authorization_request["account"]["limit"]
     transaction_amount = authorization_request["transaction"]["amount"]
     transactions_status = len(val_rules) == 0
-
-    if transactions_status:
-        account_new_limit = account_limit - transaction_amount
-    else:
-        account_new_limit = account_limit
+    account_new_limit = calculate_new_limit(transactions_status, account_limit, transaction_amount)
 
     return {
           "approved": transactions_status,
