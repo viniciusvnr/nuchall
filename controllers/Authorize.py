9f0c1059f32df20795b56d904ab34c758452ef76
viniciusvnr <viniciusvnr@gmail.com>
2019-08-21 07:59:25 -0300

    cria objeto Transaction

--- controllers/Authorize.py
+++ controllers/Authorize.py
@@ -6,14 +6,28 @@ from services.TransactionAuthorizationService import authorize_transaction
 # TODO: Logger
 
 
+class Transaction:
+    def __init__(self, request_input):
+        self.request_input = request_input
+
+        self.card_is_active = request_input["account"]["cardIsActive"]
+        self.account_limit = request_input["account"]["limit"]
+        self.deny_list = request_input["account"]["denylist"]
+        self.last_transactions = request_input["lasttransactions"]
+        self.merchant = request_input["transaction"]["merchant"]
+        self.amount = request_input["transaction"]["amount"]
+        self.time = request_input["transaction"]["time"]
+
+
 class Authorize(Resource):
     def post(self):
 
         schema = AuthorizeSchema()
+        # Valida o schema
         input_object = schema.load(request.json)
-
         if input_object.errors:
             return input_object.errors, 400
-        else:
-            do_authorization = authorize_transaction(input_object.data)
-            return do_authorization, 200
+
+        transaction_object = Transaction(input_object.data)
+        do_authorization = authorize_transaction(transaction_object)
+        return do_authorization, 200
